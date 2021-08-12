62c569d132947ced11a847335f1b89e55ae6b10b
Gustavo Gava <gustavogarciagava@gmail.com>
2021-08-12 20:16:43 -0300

    Rendering a static image

--- VideoTest/Renderer.swift
+++ VideoTest/Renderer.swift
@@ -17,7 +17,10 @@ class Renderer: NSObject {
     let metalView: MTKView
     
     var timer: Float = 0
-
+    
+    var numVertices: Int = 0
+    var viewport: vector_uint2 = [0,0]
+    let image = UIImage(named: "puppy")!
     
     init(metalView: MTKView) {
         self.metalView = metalView
@@ -28,26 +31,27 @@ class Renderer: NSObject {
         self.device = device
         metalView.device = device
         self.commandQueue = device.makeCommandQueue()!
-        
-        let mdlMesh = Primitive.makeCube(device: device, size: 1)
-        //let mdlMesh = Primitive.makePlane(device: device, size: 1)
-        do {
-            mesh = try MTKMesh(mesh: mdlMesh, device: device)
-        } catch let error {
-            print(error.localizedDescription)
-        }
-
-        vertexBuffer = mesh.vertexBuffers[0].buffer
-        
-        
         let library = device.makeDefaultLibrary()
-        let vertexFunction = library?.makeFunction(name: "vertex_main")
-        let fragmentFunction = library?.makeFunction(name: "fragment_main")
+        let vertexFunction = library?.makeFunction(name: "vertexShader")
+        let fragmentFunction = library?.makeFunction(name: "samplingShader")
         
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.vertexFunction = vertexFunction
         pipelineDescriptor.fragmentFunction = fragmentFunction
-        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlMesh.vertexDescriptor)
+        
+        var quadVertices = [
+            Vertex(position: [250, -250], textureCoordinate: [1, 1]),
+            Vertex(position: [-250, -250], textureCoordinate: [0, 1]),
+            Vertex(position: [-250, 250], textureCoordinate: [0, 0]),
+            
+            Vertex(position: [250, -250], textureCoordinate: [1, 1]),
+            Vertex(position: [-250, 250], textureCoordinate: [0, 0]),
+            Vertex(position: [250, 250], textureCoordinate: [1, 0]),
+        ]
+        
+        vertexBuffer = device.makeBuffer(bytes: &quadVertices, length: 96, options: .storageModeShared)
+        numVertices = quadVertices.count
+
         
         metalView.depthStencilPixelFormat = .invalid
         pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
@@ -66,7 +70,6 @@ class Renderer: NSObject {
     
     func createTexture() -> MTLTexture {
         //let image = UIImage(named: "puppy")!
-        let image = UIImage(color: .black)!
         let textureLoader = MTKTextureLoader(device: self.device)
         let usage: MTLTextureUsage = [.shaderRead]
         let options: [MTKTextureLoader.Option: Any] = [
@@ -75,50 +78,38 @@ class Renderer: NSObject {
         ]
         let texture = try! textureLoader.newTexture(cgImage: image.cgImage!, options: options)
         return texture
-//        let descriptor = MTLTextureDescriptor()
-//        descriptor.pixelFormat = .bgra8Unorm
-//        descriptor.width = Int(image.size.width)
-//        descriptor.height = Int(image.size.height)
-//        let texture = Renderer.device.makeTexture(descriptor: descriptor)
-        
-//        let bytesPerRow = Int(4 * image.size.width)
-//        let region = MTLRegion(
-//            origin: MTLOrigin(x: 0, y: 0, z: 0),
-//            size: MTLSize(width: Int(image.size.width), height: Int(image.size.height), depth: 1)
-//        )
-//        texture?.replace(region: region, mipmapLevel: 0, withBytes: image.cgImage!, bytesPerRow: bytesPerRow)
     }
 }
 
 extension Renderer: MTKViewDelegate {
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        self.viewport = vector_uint2(x: UInt32(size.width), y: UInt32(size.height))
     }
 
     func draw(in view: MTKView) {
-        let _ = createTexture()
+        let texture = createTexture()
         guard let descriptor = view.currentRenderPassDescriptor,
             let commandBuffer = self.commandQueue.makeCommandBuffer(),
             let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
                 return
         }
+        renderEncoder.setViewport(
+            MTLViewport(originX: 0,
+                        originY: 0,
+                        width: Double(self.viewport.x),
+                        height: Double(self.viewport.y),
+                        znear: -1,
+                        zfar: 1
+            )
+        )
         
-        timer += 0.05
-        var currentTime = sin(timer)
-        renderEncoder.setVertexBytes(&currentTime,
-                                      length: MemoryLayout<Float>.stride,
-                                      index: 1)
-
-
         renderEncoder.setRenderPipelineState(pipelineState)
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        for submesh in mesh.submeshes {
-            renderEncoder.drawIndexedPrimitives(type: .triangle,
-            indexCount: submesh.indexCount,
-            indexType: submesh.indexType,
-            indexBuffer: submesh.indexBuffer.buffer,
-            indexBufferOffset: submesh.indexBuffer.offset)
-        }
-
+        renderEncoder.setVertexBytes(&self.viewport, length: MemoryLayout.size(ofValue: self.viewport), index: 1)
+        
+        renderEncoder.setFragmentTexture(texture, index: 0)
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: numVertices)
+    
 
         renderEncoder.endEncoding()
         guard let drawable = view.currentDrawable else {
@@ -129,36 +120,4 @@ extension Renderer: MTKViewDelegate {
 
     }
 }
-
-
-class Primitive {
-    class func makeCube(device: MTLDevice, size: Float) -> MDLMesh {
-        let allocator = MTKMeshBufferAllocator(device: device)
-        let mesh = MDLMesh(boxWithExtent: [size, size, size],
-                           segments: [1, 1, 1],
-                           inwardNormals: false, geometryType: .triangles,
-                           allocator: allocator)
-        return mesh
-    }
-    
-    class func makePlane(device: MTLDevice, size: Float) -> MDLMesh {
-        let allocator = MTKMeshBufferAllocator(device: device)
-        let mesh = MDLMesh(planeWithExtent: [size, size, size], segments: [1,1], geometryType: .triangles, allocator: allocator)
-        return mesh
-    }
-}
-
-public extension UIImage {
-    convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
-        let rect = CGRect(origin: .zero, size: size)
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
-        color.setFill()
-        UIRectFill(rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        guard let cgImage = image?.cgImage else { return nil }
-        self.init(cgImage: cgImage)
-        }
-    }
 
--- VideoTest/Shaders.metal
+++ VideoTest/Shaders.metal
@@ -49,14 +49,73 @@ struct VertexIn {
   float4 position [[ attribute(0) ]];
 };
 
-// 2
-vertex float4 vertex_main(const VertexIn vertexIn [[ stage_in ]],
-                          constant float &timer [[ buffer(1) ]]) {
-  float4 position = vertexIn.position;
-  position.y += timer;
-  return position;
+struct Vertex
+{
+    // Positions in pixel space. A value of 100 indicates 100 pixels from the origin/center.
+    vector_float2 position;
+
+    // 2D texture coordinate
+    vector_float2 textureCoordinate;
+};
+
+struct RasterizerData
+{
+    // The [[position]] attribute qualifier of this member indicates this value is
+    // the clip space position of the vertex when this structure is returned from
+    // the vertex shader
+    float4 position [[position]];
+
+    // Since this member does not have a special attribute qualifier, the rasterizer
+    // will interpolate its value with values of other vertices making up the triangle
+    // and pass that interpolated value to the fragment shader for each fragment in
+    // that triangle.
+    float2 textureCoordinate;
+
+};
+
+// Vertex Function
+vertex RasterizerData
+vertexShader(uint vertexID [[ vertex_id ]],
+             constant Vertex *vertexArray [[ buffer(0) ]],
+             constant vector_uint2 *viewportSizePointer  [[ buffer(1) ]])
+
+{
+
+    RasterizerData out;
+
+    // Index into the array of positions to get the current vertex.
+    //   Positions are specified in pixel dimensions (i.e. a value of 100 is 100 pixels from
+    //   the origin)
+    float2 pixelSpacePosition = vertexArray[vertexID].position.xy;
+
+    // Get the viewport size and cast to float.
+    float2 viewportSize = float2(*viewportSizePointer);
+
+    // To convert from positions in pixel space to positions in clip-space,
+    //  divide the pixel coordinates by half the size of the viewport.
+    // Z is set to 0.0 and w to 1.0 because this is 2D sample.
+    out.position = vector_float4(0.0, 0.0, 0.0, 1.0);
+    out.position.xy = pixelSpacePosition / (viewportSize / 2.0);
+
+    // Pass the input textureCoordinate straight to the output RasterizerData. This value will be
+    //   interpolated with the other textureCoordinate values in the vertices that make up the
+    //   triangle.
+    out.textureCoordinate = vertexArray[vertexID].textureCoordinate;
+
+    return out;
 }
 
-fragment float4 fragment_main() {
-  return float4(1, 0, 0, 1);
+// Fragment function
+fragment float4
+samplingShader(RasterizerData in [[stage_in]],
+               texture2d<half> colorTexture [[ texture(0) ]])
+{
+    constexpr sampler textureSampler (mag_filter::linear,
+                                      min_filter::linear);
+
+    // Sample the texture to obtain a color
+    const half4 colorSample = colorTexture.sample(textureSampler, in.textureCoordinate);
+
+    // return the color of the texture
+    return float4(colorSample);
 }
